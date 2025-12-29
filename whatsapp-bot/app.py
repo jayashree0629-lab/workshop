7b40c5b0d79329ca7f7755df4a1b9b5c34049301
JAYASHREE <jayashree06292gmail.com>
2025-12-29 12:15:39 +0530

    first commit

---
+++ whatsapp-bot/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/whatsapp", methods=["POST"])
+def whatsapp_reply():
+    incoming_msg = request.form.get("Body", "").lower()
+
+    response = MessagingResponse()
+    msg = response.message()
+
+    if incoming_msg == "hi":
+        msg.body("Hello 👋 Welcome to your WhatsApp Bot 🤖")
+    elif incoming_msg == "help":
+        msg.body("Commands:\nhi\nhelp\ninfo")
+    elif incoming_msg == "info":
+        msg.body("This is a Python WhatsApp Automation Bot 🚀")
+    else:
+        msg.body("Sorry 😔 I didn’t understand.\nType *help*")
+
+    return str(response)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
